cc6c031f5221111b4607fb1b7ca73578a000acb9
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-06-08 14:17:51 -0300

    deploy

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export function getPokemons(limit) {
   return async function (dispatch) {
-    let json = await axios.get(`http://localhost:3002/pokemons/?limit=${limit}`);
+    let json = await axios.get(
+      `https://pablopokedexapi.herokuapp.com/pokemons/?limit=${limit}`
+    );
 
     let pagination = {
       next: json.data.next,
@@ -108,36 +110,15 @@ export function prevPage(URL) {
   };
 }
 
-
-export function getPokemonByName(name){
+export function getPokemonByName(name) {
   return async function (dispatch) {
-   
-    let json = await axios.get(`http://localhost:3002/name/?name=${name}`);
-  
-    console.log('desde actions', json.data)
-     /* let details = await Promise.all(
-      json.data.results.map(async (el) => await axios(el.url))
+    let json = await axios.get(
+      `https://pablopokedexapi.herokuapp.com/name/?name=${name}`
     );
-    details = details.map((el) => {
-      let newPokemon = {
-        id: el.data.id,
-        name: el.data.name,
-        img: el.data.sprites.other.dream_world.front_default,
-        attack: el.data.stats[1].base_stat,
-        defense: el.data.stats[2].base_stat,
-      };
-      let types = el.data.types.map((el) => el.type);
-      types.map((el) => delete el.url);
-      return (newPokemon = { ...newPokemon, types: types });
-    });
- */
-   
 
-   
- 
     return dispatch({
       type: "POKEMON_BY_NAME",
-      payload: json.data
-    }) 
+      payload: json.data,
+    });
   };
 }
